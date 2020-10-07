650e077f0ba16ca64683d913a1abfaee6dd8a746
Sang Bae <sw2.bae@gmail.com>
2020-10-07 18:16:33 -0500

    rev05_save

---
+++ build/precache-manifest.2368f42d438da5e0efe5e3d1612c9ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1aef3d644db191aacfa0ca7def2f9b",
+    "url": "/HTML-CSS-Challenge_React/index.html"
+  },
+  {
+    "revision": "e832617bc77790b81970",
+    "url": "/HTML-CSS-Challenge_React/static/css/main.8fd844ad.chunk.css"
+  },
+  {
+    "revision": "4388abc5e8ec76ee039d",
+    "url": "/HTML-CSS-Challenge_React/static/js/2.69b3a9cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HTML-CSS-Challenge_React/static/js/2.69b3a9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e832617bc77790b81970",
+    "url": "/HTML-CSS-Challenge_React/static/js/main.f7456145.chunk.js"
+  },
+  {
+    "revision": "e16d2870a91880e59608",
+    "url": "/HTML-CSS-Challenge_React/static/js/runtime-main.01f6d3ec.js"
+  }
+]);
