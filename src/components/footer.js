f751bfcb7b85563bcac49dc651b5599b9486b21b
Sang Bae <sw2.bae@gmail.com>
2020-10-08 15:25:13 -0500

    rev06_body padding-bottom Added / Footer position Fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <>
 
-            <nav className="navbar sticky-bottom bg-primary mt-5 rounded-0">
+            <nav className="navbar fixed-bottom bg-primary mt-5 rounded-0" id="footer">
                 <a className="navbar-brand text-white mx-auto" target="_blank" href="https://github.com/sw2bae/HTML-CSS-Challenge_React">"SAY WOOF!"</a>
             </nav>
         </>
