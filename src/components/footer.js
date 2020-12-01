5d28fd37755f4ef796497554c7cd68582bbe1ddf
Sang Bae <sw2.bae@gmail.com>
2020-12-01 01:01:23 -0800

    rev08_css minimized

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <>
 
             <nav className="navbar fixed-bottom bg-primary mt-5 rounded-0" id="footer">
-                <a className="navbar-brand text-white mx-auto" target="_blank" href="https://github.com/sw2bae/HTML-CSS-Challenge_React">"SAY WOOF!"</a>
+                <a className="navbar-brand text-white mx-auto" href="https://github.com/sw2bae/HTML-CSS-Challenge_React" target="_blank" rel="noopener noreferrer">"SAY WOOF!"</a>
             </nav>
         </>
     )
